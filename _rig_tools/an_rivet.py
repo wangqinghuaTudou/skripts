6802f744caa33c22ab29a8dea65d6776f81bac59
andreikin <andreikin@mail.ru>
2020-04-07 16:27:53 +0300

    1 Добавил в запуск ривета возможность делать ривет из фейса. 2 удалил мел риббон 3 Добавил в утелиты конвертр слайсов в списки

--- _rig_tools/an_rivet.py
+++ _rig_tools/an_rivet.py
@@ -1,3 +1,27 @@
+import maya.cmds as cmds
 from  an_Procedures.rivet import rivet
-def an_rivet():
-    rivet()
+from an_Procedures.utilities import an_convertSliceToList
+ 
+ 
+def getNum(comp): 
+    return int(comp.split('[')[1][:-1])
+
+def an_rivet( ):
+
+    seurface =  cmds.ls(sl=1)[0].split('.')[0]
+    sel = cmds.filterExpand (sm= 32)  #get edge
+    
+    if sel:  
+        print seurface, getNum(sel[0]), getNum(sel[1])
+        rivet(seurface, [getNum(sel[0]), getNum(sel[1])] )
+    else: 
+        sel = cmds.filterExpand (sm= 34)  #get face
+        edges = an_convertSliceToList(cmds.polyListComponentConversion( sel, te=True )) #get edges
+        data = dict()
+        for edg in edges:
+            pt = an_convertSliceToList(cmds.polyListComponentConversion( edg, tv=True ))
+            data[edg] = [ getNum(x) for x in pt ]
+        egesCapl = [x for x in edges[1:] if (data[edges[0]][0] not in data[x]) and  (data[edges[0]][1] not in data[x])]+[edges[0]]
+        print seurface, getNum(egesCapl[0]), getNum(egesCapl[1])
+        rivet(seurface, edges=[getNum(egesCapl[0]), getNum(egesCapl[1])])
+ 
--- procedures/an_Procedures/utilities.py
+++ procedures/an_Procedures/utilities.py
@@ -3,6 +3,15 @@ import maya.mel as mm
 from an_Procedures.joints import an_jntOnCurv
 
 
+'''
+        utelities
+             
+-createBonesFromEdges()        
+-an_convertSliceToList()   
+      
+'''
+
+
 def createBonesFromEdges (jntNum = 7):
     smuthnes = len(cmds.ls(sl=True))//3
     curv = mm.eval( 'polyToCurve -form 2 -degree 3;' )[0]
@@ -18,3 +27,13 @@ def createBonesFromEdges (jntNum = 7):
     cmds.makeIdentity (apply=True, t=1, r=1, s=1, n=0, pn=1)
    
     return res[0]
+    
+    
+def an_convertSliceToList(pList): #convert   "pShape.vtx[0:3]"       to     [pShape.vtx[0], pShape.vtx[1],  pShape.vtx[2], pShape.vtx[3]] 
+    output=[]
+    for pName in pList:
+        iRenge = pName.split(']')[0].split('[')[1] 
+        if ':' in iRenge:
+            for i in range ( int(iRenge.split(':')[0]), int(iRenge.split(':')[1])+1): output.append(pList[0].split('[')[0]+'['+str(i)+']' )
+        else:  output.append(pName)     
+    return output
