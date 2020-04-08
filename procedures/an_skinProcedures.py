7a85091280d9adf30108167bb104ed9316ee5c5b
andreikin <andreikin@mail.ru>
2020-04-08 16:29:39 +0300

    Привел в порядок, выкинул не нужные и добавил свап

--- procedures/an_skinProcedures.py
+++ procedures/an_skinProcedures.py
@@ -1,123 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import maya.cmds as cmds
-from  anProcedures import  * 
 
-
-"""______________an_skinProcedures_______________"""
-
-
-
-""" get  skin weight """
-def getSkinJntAndWeight( objectName):
-    skinClusterName = cmds.ls (cmds.listHistory (objectName), type='skinCluster' )[0]
-    jointName = cmds.ls (cmds.listHistory (skinClusterName, levels=1), type='transform')  ###   jnt  
-    weightList = {}                           
-    jointIndex, skinClusterIndex, skinClusterConnect, skinClusterConnectPart = [], '', '', ''  # get jnt index and weight
-    sizeArray = cmds.getAttr (objectName + ".cp", size=True )
+"""
+        an_skinProcedures
+        
+    -getSkin() 
+    -setSkin ()
+    -copySkin ()
+    -swapSkin()
+    -copySkinToSelVertex()
     
-    for i in xrange(len(jointName)):   
-        skinClusterConnect = cmds.listConnections (jointName[i] + ".worldMatrix", type='skinCluster', plugs=True )
+    -selectMissingRightSideJnt()
+    -editSelectedJntWeights():  - run paint skin tools an lock unselected jnts weights -
+    
+    - skinUi
+    
+"""
+
+
+def getSkin (geo):
+    skinCluster = cmds.ls (cmds.listHistory (geo), type='skinCluster' )[0]
+    jnts = cmds.ls (cmds.listHistory (skinCluster, levels=1), type='transform')  ###   jnt  
+    jnts = [ jnt for jnt in  jnts if  not cmds.connectionInfo(jnt+'.worldInverseMatrix[0]', isSource=True) ]
+    weight = {}                           
+    jointIndex, skinClusterIndex, skinClusterConnect, skinClusterConnectPart = [], '', '', ''  # get jnt index and weight
+    sizeArray = cmds.getAttr (geo + ".cp", size=True )
+    for i in xrange(len(jnts)):   
+        skinClusterConnect = cmds.listConnections (jnts[i] + ".worldMatrix", type='skinCluster', plugs=True )
         flag = 0
         for skinClusterIndex  in xrange(len(skinClusterConnect)):    
             skinClusterConnectPart =  skinClusterConnect[skinClusterIndex].split("[")    
-            if skinClusterName + ".matrix" == skinClusterConnectPart[0] :
+            if skinCluster + ".matrix" == skinClusterConnectPart[0] :
     			flag = 1
     			break
         jointIndex = skinClusterConnectPart[1][:-1]
-        weightList[jointName[i]] = cmds.getAttr (skinClusterName + ".weightList[0:" + str(sizeArray - 1) + "].w[" + jointIndex + "]")
-    return weightList
+        weight[jnts[i]] = cmds.getAttr (skinCluster + ".weightList[0:" + str(sizeArray - 1) + "].w[" + jointIndex + "]")
+    return weight
 
 
-""" set  skin weight """  
-def setSkinAndWeight ( objectName,  weightList):
-    jnt=[]
-    for x in  weightList.keys( ):  jnt.insert(0, x) if  cmds.objectType(x)== 'joint'  else  jnt.append( x) # sort	jnt forvard
-    newSkinClusterName = cmds.skinCluster(jnt[0], objectName, tsb=True, normalizeWeights=True)[0]  #skinning
-    cmds.setAttr (newSkinClusterName+".useComponents", 1)
+def setSkin (geo, weight):  
+    if cmds.ls (cmds.listHistory (geo), type='skinCluster' ):  #unbind skin Cluster if it exists
+        skCluster = cmds.ls (cmds.listHistory (geo), type='skinCluster' )[0]
+        cmds.skinCluster(skCluster,   e=True, unbind=True)  
+    jnt = [x for x in  weight.keys() if  cmds.objectType(x)=='joint'] + [x for x in  weight.keys() if not cmds.objectType(x)=='joint'] # sort	jnt forvard
+    skCluster = cmds.skinCluster(jnt[0], geo, tsb=True, normalizeWeights=True)[0]  #skinning
     useGeoFlag = True if  [ x for x in jnt if not cmds.objectType(x)== 'joint']  else False # influense geo test
-    cmds.skinCluster(newSkinClusterName,   e=True, useGeometry=useGeoFlag, addInfluence=jnt[1:], wt=0.0) # add influenses
-    for j in  range(len(jnt))[1:]:
-            addJntAndItsWeight(jnt[j], weightList[jnt[j]], jnt[0], objectName)
+    if useGeoFlag:  cmds.setAttr (skCluster+".useComponents", 1)
+    cmds.skinCluster(skCluster,   e=True, useGeometry=useGeoFlag, addInfluence=jnt[1:], wt=0.0) # add influenses
+    pointsList = range(len(weight[jnt[0]]))# point number list
+    jntAndPos=[] # joint and pos in claster list 
+    for jn in  jnt[1:]:  
+        jntAndPos.append([jn, [ x for x in cmds.connectionInfo(jn +'.worldMatrix[0]', dfs=True) if skCluster in x][0].split(']')[0].split ('[')[1]]) #get position in clasters jnt list
+    for jn, pos in  jntAndPos: #go through all the joints except the first one and it positions in skin claster
+        for i in pointsList: #go through all points for carrent joint 
+            if weight[jn][i]>0: # if point weight larger than 0
+                oldWeight = cmds.getAttr (skCluster + ".weightList[" +str(i)+ "].w[0]" )  # get point weight for first joint 
+                cmds.setAttr (skCluster + ".weightList[" +str(i)+ "].w[0]", oldWeight - weight[jn][i]) # correct and set point weight for first joint 
+                cmds.setAttr (skCluster + ".weightList[" +str(i)+ "].w[" + pos + "]", weight[jn][i] ) #  set point weight for carrent joint 
+    return skCluster
 
 
-""" add one jnt weight to over skin claster   """  
-def addJntAndItsWeight(impJnt, weightList, targetJnt, targetGeo):
-    skinClusterName = cmds.ls (cmds.listHistory (targetGeo), type='skinCluster' )[0]
-    jointName = cmds.ls (cmds.listHistory (skinClusterName, levels=1), type='transform')  ###   jnt  
-    if not impJnt in jointName: 
-        cmds.skinCluster(skinClusterName,   e=True, addInfluence=impJnt, wt=0.0) # add influenses
-    print skinClusterName    
-    #get position in clasters jnt list  
-    impJntWPos = [ x for x in cmds.connectionInfo(impJnt +'.worldMatrix[0]',destinationFromSource=True) if skinClusterName in x][0].split(']')[0].split ('[')[1]
-    targetJntWPos = [ x for x in cmds.connectionInfo(targetJnt +'.worldMatrix[0]',destinationFromSource=True) if skinClusterName in x][0].split(']')[0].split ('[')[1]
-    
-    for i in range(len(weightList)):
-            if weightList[i] :    
-                oldWeight = cmds.getAttr (skinClusterName + ".weightList[" +str(i)+ "].w[" + targetJntWPos + "]" ) 
-                if oldWeight>0:
-                    if  (oldWeight - weightList[i]) >0 :  
-                        cmds.setAttr (skinClusterName + ".weightList[" +str(i)+ "].w[" + targetJntWPos + "]", oldWeight - weightList[i] )
-                        cmds.setAttr (skinClusterName + ".weightList[" +str(i)+ "].w[" + impJntWPos + "]", weightList[i] )
-                    else :
-                        cmds.setAttr (skinClusterName + ".weightList[" +str(i)+ "].w[" + targetJntWPos + "]", 0.0 )
-                        cmds.setAttr (skinClusterName + ".weightList[" +str(i)+ "].w[" + impJntWPos + "]", oldWeight )
-    cmds.setAttr (skinClusterName+".maintainMaxInfluences", 0)
-
-
-""" insert several jnts and its weight to one jnt """  
-def insertJntAndWeight():
-    sl1, sl2 = cmds.ls(sl=True)
-    copyGeo= cmds.duplicate (sl2)[0]
-    an_copySkin( sl1, copyGeo)
-    
-    serseGeo, targetGeo = copyGeo, sl2
-    
-    #serseGeo, targetGeo = cmds.ls(sl=True)
-    serseGeoWeight = getSkinJntAndWeight( serseGeo)
-    targetGeoWeight = getSkinJntAndWeight( targetGeo)
-
-    targetJnt =[]
-    for jnt in targetGeoWeight.keys(): 
-        if jnt in serseGeoWeight.keys(): targetJnt.append(jnt)
+def copySkin(geo, dest_geo ):
+    sourceSkin = cmds.ls(cmds.listHistory(geo, pruneDagObjects=True) , type='skinCluster')[0]
+    influences = cmds.skinCluster(sourceSkin, query=True, influence=True)
+    joints = cmds.ls(influences, type='joint') # getting joints list on source skinCluster
+    nurbs = list(set(influences) - set(joints)) # getting nurbs list on source skinCluster
+    useComp = cmds.getAttr(sourceSkin + '.useComponents') # getting state of "useComponents" attribute on source skinCluster
+    hist = cmds.listHistory(dest_geo, pruneDagObjects=True) # try to find history on the destination object
+    dest_geoSkin = cmds.ls(hist, type='skinCluster')[0]  if cmds.ls(hist, type='skinCluster') else None# try to find skinCluster on the destination object
+    if dest_geoSkin: cmds.skinCluster(dest_geoSkin, e=True, unbind=True) # unbind skinCluster with deleting history
+    tempJoint = None   # create new skinCluster on destination object with joints influences
+    if not joints: 
+        cmds.select(clear=True)
+        tempJoint = cmds.joint()
+        joints = tempJoint
+    destSkin = cmds.skinCluster(dest_geo, joints, toSelectedBones=True, useGeometry=True, dropoffRate=4, polySmoothness=False, nurbsSamples=25, rui=False, mi=5, omi =False, normalizeWeights=True)[0]                            
+    if nurbs:   # add nurbs influences in new skinCluster
+        cmds.skinCluster(destSkin, edit=True, useGeometry=True, dropoffRate=4, polySmoothness=False, nurbsSamples=25, lockWeights=False, weight=0, addInfluence=nurbs) 
+    cmds.setAttr((destSkin + '.useComponents'), useComp) # set state for useComponets attribute
+    # copy skin weights from source object to destination
+    cmds.copySkinWeights(sourceSkin=sourceSkin, destinationSkin=destSkin, noMirror=True, surfaceAssociation='closestPoint', influenceAssociation='oneToOne', normalize=True)
+    if tempJoint: cmds.delete(tempJoint) # clear template joints
+          
+    if  cmds.getAttr('%s.deformUserNormals'%destSkin): # setting up userNormals
+        cmds.setAttr('%s.deformUserNormals'%destSkin, 0)
   
-    for jnt in serseGeoWeight.keys():
-        if not jnt==targetJnt[0]:
-            addJntAndItsWeight(jnt, serseGeoWeight[jnt], targetJnt[0], targetGeo)
-    cmds.delete (copyGeo)
 
-"""
-def doInsert():
-    serseGeo, targetGeo = cmds.ls(sl=True)
-    copyGeo= cmds.duplicate (targetGeo)[0]
-    an_copySkin( serseGeo, copyGeo)
-    cmds.select (copyGeo, targetGeo)
-    insertJntAndWeight()
-    cmds.delete (copyGeo)
+
+def swapSkin( s_geo='', t_geo='', setSkinWeights=True ): #source_geo, target_geo
+    if not s_geo:
+        s_geo, t_geo = cmds.ls(sl=True)
+    s_geo_weight = getSkin(s_geo)
+    t_geo_weight = getSkin(t_geo)
+    base_jnt = [x for x in s_geo_weight.keys() if sum(s_geo_weight[x])==0][0]
+    mutualJoints = list(set(s_geo_weight.keys())  &  set(t_geo_weight.keys())) 
+    
+    if len(mutualJoints)==1: #insert
+        copySkin(s_geo, t_geo)
+        pasted_weight = getSkin(t_geo)
+        base_jnt_weight = t_geo_weight.pop(base_jnt)     
+        for jnt in pasted_weight.keys(): 
+                t_geo_weight[jnt]= [pasted_weight[jnt][i]*base_jnt_weight[i] for i in  range( len(base_jnt_weight))] 
+        if setSkinWeights:
+            setSkin(t_geo, t_geo_weight)
+            cmds.select(s_geo, t_geo) 
+        return t_geo_weight
+    else:                    #extract
+        jnts = [x for x in s_geo_weight.keys() if not x==base_jnt]
+        for v in xrange(len(t_geo_weight[base_jnt])) :
+            t_geo_weight[base_jnt][v] = sum([t_geo_weight[jn][v] for jn in jnts ])
+        for jn in jnts:
+            t_geo_weight.pop(jn) 
+        if setSkinWeights:
+            setSkin(t_geo, t_geo_weight) 
+            cmds.select(s_geo, t_geo)
+        return t_geo_weight
+
+def selectMissingRightSideJnt():
+    objectName = cmds.ls(sl=True)[0]
+    skinClusterName =  cmds.ls (cmds.listHistory (objectName, pdo=1), type='skinCluster')[0]    ### if claster sel
+    history = cmds.listHistory (objectName)
+    clustersName = cmds.ls (history, type='skinCluster' )
+    skinClusterSetName = cmds.listConnections (skinClusterName, type='objectSet' )
+    jointName = cmds.ls (cmds.listHistory (skinClusterName, levels=1), type='transform')  
+    rSidJnt = ['r_'+x[2:] for x in jointName if 'l_'==x[:2]]
+    missingJnt = []
+    for jnt in  rSidJnt:
+        if not jnt in jointName:
+             missingJnt.append(jnt)
+    cmds.select(missingJnt)
 
  
-def insertJntAndWeight():
-    serseGeo, targetGeo = cmds.ls(sl=True)
-    serseGeoWeight = getSkinJntAndWeight( serseGeo)
-    targetGeoWeight = getSkinJntAndWeight( targetGeo)
-
-    targetJnt =[]
-    for jnt in targetGeoWeight.keys(): 
-        if jnt in serseGeoWeight.keys(): targetJnt.append(jnt)
-  
-    for jnt in serseGeoWeight.keys():
-        if not jnt==targetJnt[0]:
-            addJntAndItsWeight(jnt, serseGeoWeight[jnt], targetJnt[0], targetGeo)
-
-"""
-
-
-
-""" run paint skin tools an lock unselected jnts weights """  
-def editSelectedJntWeights():  # select jnt then geo
+def editSelectedJntWeights():  
     geo = cmds.ls(sl=True)[-1]
     jnts = cmds.ls(sl=True)[:-1]
     
     skinClusterV = cmds.ls (cmds.listHistory (geo), type='skinCluster' )[0]
-    jointName = cmds.ls (cmds.listHistory (skinClusterV, levels=1), type='transform')  ###   jnt  
+    jointName = cmds.ls (cmds.listHistory (skinClusterV, levels=1), type='transform')   
  
     for jnt in [x for x in jointName if not x in jnts]:  cmds.setAttr (jnt+'.liw', 1)
     for jnt in  jnts:  cmds.setAttr (jnt+'.liw', 0)
@@ -127,75 +147,7 @@ def editSelectedJntWeights():  # select jnt then geo
     mm.eval("ArtPaintSkinWeightsToolOptions;")
     mm.eval('setSmoothSkinInfluence '+jnts[0]+' ;artSkinRevealSelected artAttrSkinPaintCtx;')
 
-
-""" copy skin weight from one object to another """  
-def an_copySkin( sourceObj, destObj ):  
-    #sourceObj, destObj = cmds.ls(selection=True)
-    sourceSkin = cmds.ls(cmds.listHistory(sourceObj, pruneDagObjects=True) , type='skinCluster')[0]
-    influences = cmds.skinCluster(sourceSkin, query=True, influence=True)
-    joints = cmds.ls(influences, type='joint') # getting joints list on source skinCluster
-    nurbs = list(set(influences) - set(joints)) # getting nurbs list on source skinCluster
-    useComp = cmds.getAttr(sourceSkin + '.useComponents') # getting state of "useComponents" attribute on source skinCluster
-
-    hist = cmds.listHistory(destObj, pruneDagObjects=True) # try to find history on the destination object
-    destObjSkin = cmds.ls(hist, type='skinCluster')[0]  if cmds.ls(hist, type='skinCluster') else None# try to find skinCluster on the destination object
-    if destObjSkin: cmds.skinCluster(destObjSkin, e=True, unbind=True) # unbind skinCluster with deleting history
-    tempJoint = None   # create new skinCluster on destination object with joints influences
-    if not joints: 
-        cmds.select(clear=True)
-        tempJoint = cmds.joint()
-        joints = tempJoint
-        
-    destSkin = cmds.skinCluster(destObj, joints, toSelectedBones=True, useGeometry=True, dropoffRate=4, polySmoothness=False, nurbsSamples=25, rui=False, mi=5, omi =False, normalizeWeights=True)[0]                            
-    if nurbs:   # add nurbs influences in new skinCluster
-        cmds.skinCluster(destSkin, edit=True, useGeometry=True, dropoffRate=4, polySmoothness=False, nurbsSamples=25, lockWeights=False, weight=0, addInfluence=nurbs) 
-    cmds.setAttr((destSkin + '.useComponents'), useComp) # set state for useComponets attribute
-    # copy skin weights from source object to destination
-    cmds.copySkinWeights(sourceSkin=sourceSkin, destinationSkin=destSkin, noMirror=True, surfaceAssociation='closestPoint', influenceAssociation='oneToOne', normalize=True)
-    
-    if tempJoint: cmds.delete(tempJoint) # clear template joints
-          
-    if  cmds.getAttr('%s.deformUserNormals'%destSkin): # setting up userNormals
-        cmds.setAttr('%s.deformUserNormals'%destSkin, 0)
-    cmds.select(destObj, r=True) # selecting destination object
-
  
-def combineSkinParts():     # connect parts of skin to one  
-    geo = 'body____skin'
-    corpus,  arm, leg, hand = 'corpus____skin',  'arm____skin', 'leg____skin', 'hand____skin' 
-    
-    an_copySkin( corpus, geo) 
-    
-    cmds.select(arm, geo)
-    insertJntAndWeight()
-    
-    cmds.select(leg, geo)
-    insertJntAndWeight()
-    
-    cmds.select(hand, geo)
-    insertJntAndWeight()
+ 
 
-    cmds.select(geo)              #add right joints
-    selectMissingRightSideJnt ()
-    r_jnts = cmds.ls(sl=True)
-    skinClusterName = cmds.ls (cmds.listHistory (geo), type='skinCluster' )[0]
-    cmds.skinCluster(skinClusterName,   e=True, addInfluence=r_jnts, wt=0.0)
-
-    cmds.copySkinWeights (ss=skinClusterName, ds=skinClusterName, mirrorMode='YZ', surfaceAssociation='closestPoint', influenceAssociation='closestJoint')
-
-
-def selectMissingRightSideJnt ():
-    objectName = cmds.ls(sl=True)[0]
-    skinClusterName =  cmds.ls (cmds.listHistory (objectName, pdo=1), type='skinCluster')[0]    ### if claster sel
-    history = cmds.listHistory (objectName)
-    clustersName = cmds.ls (history, type='skinCluster' )
-    skinClusterSetName = cmds.listConnections (skinClusterName, type='objectSet' )
-    jointName = cmds.ls (cmds.listHistory (skinClusterName, levels=1), type='transform')  ###   jnt
-    rSidJnt = ['r_'+x[2:] for x in jointName if 'l_'==x[:2]]
-    missingJnt = []
-    for jnt in  rSidJnt:
-        if not jnt in jointName:
-             missingJnt.append(jnt)
-    cmds.select(missingJnt)
-    
     
