c28625ec34d93ae0e61d0a069310bf3f4cc9a297
andreikin <andreikin@mail.ru>
2021-01-30 18:15:02 +0300

    dynamics

--- dynamics/hair_strand_rig.py
+++ dynamics/hair_strand_rig.py
@@ -25,9 +25,13 @@ HELP_TEXT = "\n" \
             "4 The root joint must have a parent, to which the system will be attached later\n" \
             "5 Choose 'Create dynanmics sistem'\n" \
 
-MAX_VERTEX_NUM = 40
-MAX_JOINT_NUM = 40
 
+
+DEFOULT_PREFIX = "haersStrand"
+MAX_VERTEX_NUM = 40
+DEFOULT_VERTEX_NUM = 4
+MAX_JOINT_NUM = 40
+DEFOULT_JOINT_NUM = 12
 
 class HairStrand_UI(QMainWindow):
     def __init__(self):
@@ -79,7 +83,7 @@ class HairStrand_UI(QMainWindow):
         self.pfx_layout = QHBoxLayout()
         self.pfx_label = QLabel("Prefix:")
         self.pfx_layout.addWidget(self.pfx_label)
-        self.pfx_lineEdit = QLineEdit(self.option_box)
+        self.pfx_lineEdit = QLineEdit(DEFOULT_PREFIX)
         self.pfx_layout.addWidget(self.pfx_lineEdit)
         self.option_box_layout.addLayout(self.pfx_layout)
         # slider Points number
@@ -87,16 +91,16 @@ class HairStrand_UI(QMainWindow):
         self.curve_label = QLabel("Points number :")
         self.curve_hLayout.addWidget(self.curve_label)
         self.curve_horizontal_slider = QSlider()
-        self.curve_horizontal_slider.setMinimum(3)
+        self.curve_horizontal_slider.setMinimum(1)
         self.curve_horizontal_slider.setMaximum(MAX_VERTEX_NUM)
-        self.curve_horizontal_slider.setValue(10)
-        self.curve_horizontal_slider.setSliderPosition(10)
+        #self.curve_horizontal_slider.setValue(DEFOULT_VERTEX_NUM)
+        self.curve_horizontal_slider.setSliderPosition(DEFOULT_VERTEX_NUM)
         self.curve_horizontal_slider.setOrientation(Qt.Horizontal)
         self.curve_hLayout.addWidget(self.curve_horizontal_slider)
         self.curve_spin_box = QSpinBox()
-        self.curve_spin_box.setMinimum(3)
+        self.curve_spin_box.setMinimum(1)
         self.curve_spin_box.setMaximum(MAX_VERTEX_NUM)
-        self.curve_spin_box.setValue(10)
+        self.curve_spin_box.setValue(DEFOULT_VERTEX_NUM)
         self.curve_hLayout.addWidget(self.curve_spin_box)
         self.curve_horizontal_slider.valueChanged.connect(self.curve_spin_box.setValue)
         self.curve_spin_box.valueChanged.connect(self.curve_horizontal_slider.setValue)
@@ -108,14 +112,14 @@ class HairStrand_UI(QMainWindow):
         self.joints_horizontal_slider = QSlider()
         self.joints_horizontal_slider.setMinimum(3)
         self.joints_horizontal_slider.setMaximum(MAX_JOINT_NUM)
-        self.joints_horizontal_slider.setSliderPosition(10)
+        self.joints_horizontal_slider.setSliderPosition(DEFOULT_JOINT_NUM)
         self.joints_horizontal_slider.setOrientation(Qt.Horizontal)
         self.horizontalLayout_2.addWidget(self.joints_horizontal_slider)
         self.joints_spin_box = QSpinBox()
         self.joints_spin_box.setObjectName(u"joints_spin_box")
         self.joints_spin_box.setMinimum(3)
         self.joints_spin_box.setMaximum(MAX_JOINT_NUM)
-        self.joints_spin_box.setValue(10)
+        self.joints_spin_box.setValue(DEFOULT_JOINT_NUM)
         self.horizontalLayout_2.addWidget(self.joints_spin_box)
         self.joints_horizontal_slider.valueChanged.connect(self.joints_spin_box.setValue)
         self.joints_spin_box.valueChanged.connect(self.joints_horizontal_slider.setValue)
@@ -133,60 +137,93 @@ class HairStrand_UI(QMainWindow):
         self.verticalLayout.addLayout(self.buttons_layout)
         self.create_button.clicked.connect(self.create_rig)
 
-    def get_data_from_ui(self):
-        self.prefx = self.pfx_lineEdit.text()
-        self.point_number = self.curve_horizontal_slider.value()
-        self.joint_number = self.joints_horizontal_slider.value()
-        return self.prefx, self.point_number, self.joint_number
+
 
 
 class HairStrandRig(HairStrand_UI):
-    def __init__(self):
-        super(HairStrandRig, self).__init__()
-        
-        
-    def get_objects_and_chek_it(self):
-        self.temp_joints = cmds.ls(sl=True)
-        if len(self.temp_joints)<3:
-            logging.getLogger().error("Necessary to select at least three joints")
-            return False
-        # get an object to which the whole system is attached by constraint
-        try:
-            self.parent_object = cmds.listRelatives(self.temp_joints[0], parent = True)[0]
-        except TypeError:
-            self.parent_object = None
-            logging.getLogger().warning("No parent, so the system will not be attached!")
-        return True
-    
     
     def create_rig(self):
         self.get_data_from_ui()
         if self.get_objects_and_chek_it():
             self.rig_grp = cmds.group(empty = True, name = self.prefx+'Rig_grp')
-            self.controllers_placement()
-            self.curve = self.building_basic_curve()
-            self.dynamic_curve = self.building_dynamic_curve()
-     
-    def controllers_placement(self):
+            self.fk_rigging()
+            self.building_base_curve()
+            self.building_dynamic_curve()
+            
+    def get_data_from_ui(self):
+        self.prefx = self.pfx_lineEdit.text()
+        if not self.prefx:
+            logging.getLogger().warning("You did not enter a prefix, the default value will be used !")
+            self.prefx = DEFOULT_PREFIX   
+        self.point_number = self.curve_horizontal_slider.value()
+        self.joint_number = self.joints_horizontal_slider.value()
+  
+    def get_objects_and_chek_it(self):
+        self.ctrl_joints = cmds.ls(sl=True)
+        if len(self.ctrl_joints)<3:
+            logging.getLogger().error("Necessary to select at least three joints")
+            return False
+        # get an object to which the whole system is attached by constraint
+        try:
+            self.parent_object = cmds.listRelatives(self.ctrl_joints[0], parent = True)[0]
+        except TypeError:
+            self.parent_object = None
+            logging.getLogger().warning("No parent, so the system will not be attached!")
+        return True
+    
+    def fk_rigging(self):
         self.controls=[]
         self.fk_grp = cmds.group(empty = True, name = self.prefx+'FK_grp')
         cmds.parent(self.fk_grp, self.rig_grp)
         if self.parent_object:
-            cmds.parentConstraint(self.parent_object, self.fk_grp) 
-        for i, tmp_jnt in enumerate(self.temp_joints):
+            cmds.parentConstraint(self.parent_object, self.fk_grp)
+        for i, jnt in enumerate(self.ctrl_joints[:-1]):
             ctrl = AnControllers(self.prefx+str(i)+"_CT")
-            ctrl.makeController(shapeType= "fk", orient="X", pos=tmp_jnt )
+            ctrl.makeController(shapeType= "fk", orient="X", pos=jnt )
             self.controls.append(ctrl)
             if i == 0:
                 cmds.parent(ctrl.oriGrp, self.fk_grp) 
             else:
                 cmds.parent(ctrl.oriGrp, self.controls[i-1].name) 
+            cmds.parentConstraint(ctrl.name, jnt)
            
-    def building_basic_curve(self):
-        return None
+    def building_base_curve(self):
+        point_position = []
+        for jnt in self.ctrl_joints:
+            jnt_coordinates = cmds.xform(jnt, query=True, translation=True, worldSpace=True)
+            point_position.append(jnt_coordinates)
+        self.base_curve = cmds.curve(p=point_position, degree=2, name=self.prefx+'FK_crv')
         
+        skCluster = cmds.skinCluster(self.ctrl_joints, self.base_curve, toSelectedBones=True, normalizeWeights=True)[0] 
+        cmds.rebuildCurve(self.base_curve, 
+                            rebuildType=0, 
+                            spans=self.point_number, 
+                            constructionHistory=True)
+        cmds.parent(self.base_curve, self.rig_grp)
+
     def building_dynamic_curve(self):
-        return None
+        self.input_curve = cmds.duplicate(name=self.prefx+'Input_crv', inputConnections=True)
+        
+        mm.eval('makeCurvesDynamic 2 { "0", "0", "1", "1", "0"};')
+        crv_shape= cmds.listRelatives (self.input_curve, shapes=True)[1]
+        print crv_shape
+        folicle = cmds.listConnections (crv_shape+".local") [0]
+        cmds.setAttr (folicle+".pointLock", 0)
+        folShape = cmds.listRelatives (folicle, shapes=True)[0]
+        dynCurveShape = cmds.connectionInfo (folShape+".outCurve", destinationFromSource=True)[0].split('.')[0]   
+        self.dynCurve = cmds.listRelatives (dynCurveShape, parent=True)[0]		 
+        self.hairSysShape = cmds.connectionInfo (folShape+".outHair", destinationFromSource=True)[0].split('.')[0] 
+        hairSys = cmds.listRelatives (self.hairSysShape, parent=True)[0] 
+        dynCurveGrp = cmds.listRelatives (self.dynCurve, parent=True)[0]
+        self.dynCurve = cmds.rename (self.dynCurve,  self.prefx+'Dynamics_crv')
+        cmds.parent(hairSys, dynCurveGrp, self.rig_grp)
+        
+        
+        
+        cmds.select (self.dynCurve+'.cv[0:2]')
+        dyn_constraint = mm.eval('createNConstraint transform 0;')
+        print dyn_constraint
+        
         
     def delete_rig(self):
         print ("delete_rig")
