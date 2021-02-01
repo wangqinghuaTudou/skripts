51b84e03d58bec8588a40623015ca061594a72cd
andreikin <andreikin@mail.ru>
2021-02-01 16:31:44 +0300

    add functions jntOnCurvNonSpline

--- procedures/an_Procedures/joints.py
+++ procedures/an_Procedures/joints.py
@@ -4,7 +4,8 @@ from an_Procedures.utilities import an_delSys
 '''
         joints
 -createBonesFromEdges ()           
--an_convertSliceToList()   
+-an_jntOnCurv() 
+-jntOnCurvNonSpline()  
       
 '''
 
@@ -59,8 +60,60 @@ def an_jntOnCurv(curveName, jntNum = 10, stretchable=True, pfx='', geo=False):
     else: an_delSys(ikHandl[0], objList =[jointName[0], ])
     return jointName, ikHandl
     
+ 
+def jntOnCurvNonSpline(curve_name, jnt_num=10, pfx='', geo=True):
     
+    pfx = curve_name + 'JntOnCurve' if not pfx else pfx
+    curve_shape = cmds.listRelatives(curve_name, s=True)[0]
+    jointsNames = range(jnt_num + 1)
+    rigGrp = cmds.group(n=pfx + 'Joints_grp', em=True)
+    jointsNames = range(jnt_num)
+    curve_len = cmds.arclen(curve_name)
+    
+    cmds.select(cl=True, sym=True)
+    for i, string in enumerate(range(jnt_num)):
+        jointsNames[i] = cmds.joint(relative=True,
+                                  name=pfx + str(i) + '_jnt',
+                                  position=[curve_len / (jnt_num - 1), 0, 0])
+    if geo:
+        geo = cmds.polyCylinder(name=pfx + 'Skin_geo',
+                                radius=curve_len / jnt_num / 8,
+                                height=curve_len,
+                                subdivisionsY=jnt_num - 1,
+                                subdivisionsX=6,
+                                axis=[1, 0, 0])[0]
+        cmds.delete(cmds.pointConstraint(jointsNames[0], jointsNames[-1], geo))
+        cmds.skinCluster(jointsNames, geo, tsb=True, normalizeWeights=True)
+        cmds.parent(geo, rigGrp)
 
+    for i, string in enumerate(range(jnt_num)):
+        cmds.select(cl=True, sym=True)
+        cmds.parent(jointsNames[i], rigGrp)
+        point_on_curve_info = cmds.createNode('pointOnCurveInfo', n=pfx + str(i) + '_poci')
+        cmds.setAttr(point_on_curve_info + ".turnOnPercentage", 1)
+        cmds.setAttr(point_on_curve_info + ".parameter", 1.0 / (jnt_num - 1) * i)
+        cmds.connectAttr(curve_shape + '.worldSpace[0]', point_on_curve_info + '.inputCurve')
+        cmds.connectAttr(point_on_curve_info + '.position', jointsNames[i] + '.translate')
+        if not i == 0:
+            cmds.aimConstraint(jointsNames[i - 1], jointsNames[i],
+                               aim=[-1, 0, 0],
+                               upVector=[0, 0, 1],
+                               worldUpVector=[0, 0, 1],
+                               worldUpType='objectrotation',
+                               worldUpObject=jointsNames[i - 1])
+    loc = cmds.spaceLocator(name=pfx + 'Up_loc')[0]
+    cmds.parent(loc, jointsNames[0])
+    for attr in ['tx', 'ty', 'tz']:    cmds.setAttr(loc + '.' + attr, 0)
+    cmds.setAttr(loc + '.tz', curve_len / (jnt_num - 1))
+    cmds.parent(loc, w=True)
+    cmds.aimConstraint(jointsNames[1], jointsNames[0],
+                       aim=[1, 0, 0],
+                       upVector=[0, 0, 1],
+                       worldUpVector=[0, 0, 1],
+                       worldUpType='object',
+                       worldUpObject=loc)
+    cmds.parent(loc, rigGrp)
+    return loc, jointsNames, rigGrp
 
 
 
