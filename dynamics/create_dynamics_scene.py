f5576cb4a81db99f8be1e6c12a86a4629b935a6b
andreikin <andreikin@mail.ru>
2021-08-06 14:28:14 +0300

    start

--- dynamics/create_dynamics_scene.py
+++ dynamics/create_dynamics_scene.py
@@ -1,13 +1,12 @@
-
 import re
-
 
 IGNOR_LIST = "general_CT", "pivotOffset_CT", "switch_CT"
 CHAR_URL = [
-            u'//renderServer/Project/SOBAKI_3d/assets/chars/liza/maya/work/rig/liza_rig.mb',
-            u'C:/Users/belyaev_a/Desktop/anim_for_cloth/liza_v041.mb'
-            ]
+    u'//renderServer/Project/SOBAKI_3d/assets/chars/liza/maya/work/rig/liza_rig.mb',
+    u'C:/Users/belyaev_a/Desktop/anim_for_cloth/liza_v041.mb'
+]
 INPUT_OFFSET = -20
+#RELACS
 
 def set_t_pos(reference, ct_pattern="[\w:_]+_CT$"):
     """
@@ -37,7 +36,7 @@ def set_t_pos(reference, ct_pattern="[\w:_]+_CT$"):
                 cmds.setAttr(ctrl + "." + attr, default_val)
             except Exception as exception_data:
                 print(exception_data)
-  
+
     # create locators in control position
     locatots = []
     for ctrl in node_list:
@@ -70,7 +69,7 @@ def set_t_pos(reference, ct_pattern="[\w:_]+_CT$"):
                 final_values.append([ctrl, attr, val])
             except Exception as exception_data:
                 print(exception_data)
-                
+
     cmds.delete(locatots)
 
     # set final attributes values
@@ -82,77 +81,61 @@ def set_t_pos(reference, ct_pattern="[\w:_]+_CT$"):
             print(exception_data)
 
 
-
-
-def get_all_controls(ctrl, sfx = "_CT"):
+def get_all_controls(ctrl, sfx="_CT"):
     try:
-        name_space = cmds.referenceQuery(ctrl, namespace=True , shortName=True)+":"
+        name_space = cmds.referenceQuery(ctrl, namespace=True, shortName=True) + ":"
     except RuntimeError:
-        name_space =""
-        
+        name_space = ""
+
     formula = re.compile(name_space + "\w+_CT$")
     return [x for x in cmds.ls() if formula.match(x)]
 
 
-
-
 def create_dynamics_scene(CHAR_URL):
-    
     reference_list = cmds.file(query=True, reference=True)
-    characters_list = [cmds.referenceQuery(x, namespace=True , shortName=True) for x in reference_list if x in CHAR_URL]
+    characters_list = [cmds.referenceQuery(x, namespace=True, shortName=True) for x in reference_list if x in CHAR_URL]
     all_controls = []
     for char in characters_list:
         char_ignor_list = [char + ":" + ct for ct in IGNOR_LIST]
         ctrl = [ct for ct in get_all_controls(char + ":" + IGNOR_LIST[0]) if ct not in char_ignor_list]
         all_controls += ctrl
-    
+
     start_time = int(cmds.playbackOptions(q=True, min=True))
     end_time = int(cmds.playbackOptions(q=True, max=True))
     cmds.bakeResults(all_controls,
-                     simulation = True,
-                     time = (start_time, end_time),
-                     sampleBy = 1,
-                     oversamplingRate = 1,
-                     disableImplicitControl = True,
-                     preserveOutsideKeys = True,
-                     sparseAnimCurveBake = False,
-                     removeBakedAttributeFromLayer = False,
-                     removeBakedAnimFromLayer = False,
-                     bakeOnOverrideLayer = False,
-                     minimizeRotation = True,
-                     controlPoints = False,
-                     shape = True)
-                     
-    cmds.playbackOptions( min=start_time-10)
-    
+                     simulation=True,
+                     time=(start_time, end_time),
+                     sampleBy=1,
+                     oversamplingRate=1,
+                     disableImplicitControl=True,
+                     preserveOutsideKeys=True,
+                     sparseAnimCurveBake=False,
+                     removeBakedAttributeFromLayer=False,
+                     removeBakedAnimFromLayer=False,
+                     bakeOnOverrideLayer=False,
+                     minimizeRotation=True,
+                     controlPoints=False,
+                     shape=True)
+
+    cmds.playbackOptions(min=start_time - 10)
+
     for ct in all_controls:
         try:
-            cmds.copyKey( ct, time=(start_time, start_time)  )             
-            cmds.pasteKey( ct, time=(start_time-10, start_time-10))                 
+            cmds.copyKey(ct, time=(start_time, start_time))
+            cmds.pasteKey(ct, time=(start_time - 10, start_time - 10))
         except Exception as exception_data:
             print (exception_data)
-    
-    cmds.playbackOptions( min=start_time + INPUT_OFFSET)
+
+    cmds.playbackOptions(min=start_time + INPUT_OFFSET)
     cmds.currentTime(start_time + INPUT_OFFSET, edit=True)
-    
+
     for ref in reference_list:
         reference = cmds.file(ref, query=True, referenceNode=True)
         print reference
-        
+
         set_t_pos(reference)
-    
-    cmds.keyTangent (all_controls, itt="linear", ott="linear")
+
+    cmds.keyTangent(all_controls, itt="linear", ott="linear")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+create_dynamics_scene(CHAR_URL)
